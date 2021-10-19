4309b5d660a9edefe4063cc1f10c6ec988481746
Jon K <Johnakao@gmail.com>
2021-10-19 11:22:10 -0700

    Cleaned slate/removed started files

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChFE1vS12_DgnomyCBjw6O87ChOMJxIQY",
+  authDomain: "j-clone-5288c.firebaseapp.com",
+  projectId: "j-clone-5288c",
+  storageBucket: "j-clone-5288c.appspot.com",
+  messagingSenderId: "101714891521",
+  appId: "1:101714891521:web:47feff310f43e5b6410100",
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
