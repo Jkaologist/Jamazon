7a3778bae18f330b8ac18c7cd324137fb2d83a59
Jon K <Johnakao@gmail.com>
2021-10-19 13:35:23 -0700

    Fixed dependencies bug and SVG file styling

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "./jamazon.jpg";
+import SearchIcon from "@mui/icons-material/Search";
 import "./Header.css";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 
 function Header() {
   return (
@@ -9,20 +11,26 @@ function Header() {
 
       <div className="header__search">
         <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
       </div>
 
       <div className="header__nav">
         <div className="header__option">
-          <span className="header__optionLineOne"> Hello Guest </span>
-          <span className="header__optionLineTwo"> Sign In </span>
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
         </div>
         <div className="header__option">
-          <span className="header__optionLineOne"> Returns </span>
-          <span className="header__optionLineTwo"> & Orders </span>
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
         </div>
         <div className="header__option">
-          <span className="header__optionLineOne"> Your </span>
-          <span className="header__optionLineTwo"> Prime </span>
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon sx={{ color: "white" }} />
+          <span className="header__optionLineTwo header__basketCount">100</span>
         </div>
       </div>
     </div>
